952f69bc1b8ba0772f207553463a78f1987d69a5
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-01-31 08:18:18 -0600

    Uploads a photo

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,25 +1,33 @@
 
 
 import Foundation
-import PureURLSession
 
 
-func runNetwork() {
+func uploadImage( oncomplete: @escaping (Void)->Void ) {
 
-     let config = PureURLSession.URLSessionConfiguration.default // Session Configuration
-     let mySession = PureURLSession.URLSession(configuration: config) // Load configuration into Session
+     let config = URLSessionConfiguration.default // Session Configuration
+     let mySession = URLSession(configuration: config) // Load configuration into Session
 
-     guard let url = URL(string: "http://www.google.com") else {
+     guard let data = try? Data(contentsOf: URL(fileURLWithPath: "IBM.png")) else {
+     	fatalError("Could not load the data to upload")
+     }
+
+     guard let url = URL(string: "http://localhost:3000") else {
      	   fatalError("Could not create URL")
 	   }
 
 
+	   var request = URLRequest(url: url)
+	   request.httpMethod = "POST"
+	   request.httpBody = data 
 
-	   let task = mySession.dataTask(with: url) {
+	   let task = mySession.dataTask(with: request) {
             (data, response, error) in
 
 	    print("Received a response")
 	    print("data is \(data!)")
+
+	    oncomplete()
 
 
  	    }
@@ -29,7 +37,14 @@ func runNetwork() {
 
 }
 
-print("Running a network request")
-runNetwork()
+
+print("Uploading the image")
+
+uploadImage() {
+
+	print("Done")
+
+}
+
 
 sleep(5)
